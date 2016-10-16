a5440dac4f2dc84033c9c13c640cd9a19f038048
Matt Diener <mattndiener@gmail.com>
2016-10-16 00:45:50 -0400

    First pass at createBoard

--- frontend/app/pods/index/controller.js
+++ frontend/app/pods/index/controller.js
@@ -25,26 +25,26 @@ export default Ember.Controller.extend({
 				action: ' sign in',
 				on: 'onclick'
 				});
-				
-		
+
+
         // create sidebar and attach to menu open
         Ember.$('.ui.sidebar')
           .sidebar('attach events', '.toc.item');
 	  });
   },
-  
-  
+
+
 	// Get Token?
 	session: Ember.inject.service('session'),
 	actions:{
-		
+
 		goToBoard() {
 			const token = this.get('boardToken');
 			if (token) {
 				this.send('goToBoardRoute', token);
 			}
 		},
-		
+
 		createBoard() {
 			const urlToPost = "/boards";
 			const opts = {
@@ -55,8 +55,8 @@ export default Ember.Controller.extend({
 			};
 			const self = this;
 			Ember.$.ajax(opts).then((results) => {
-				this.transitionToRout('board/view/'+ results.board.boardId);
-				
+				this.transitionToRoute('board.view', results.board.boardId);
+
 			});
 		}
 	}
